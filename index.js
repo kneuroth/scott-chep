04cf21ce1d9cfeea48ebb0fdbe6c41754395a714
Kelly N <kelly.neuroth@outlook.com>
2025-03-06 17:13:17 -0500

    Home and 35 pages

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // app.js
 const express = require("express");
 const path = require("path");
+const { mainModule } = require("process");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 app.get("/35", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/35", "index.html"));
+  res.sendFile(path.join(__dirname, "public/35", "main.html"));
+});
+
+app.get("/wrong", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/wrong", "main.html"));
 });
 
 app.listen(3000, () => {
