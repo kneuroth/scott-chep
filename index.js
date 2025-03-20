9e510a1d26de881f751dac18e23032b333170cf9
Kelly N <kelly.neuroth@outlook.com>
2025-03-20 17:31:52 -0400

    rename

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "home.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/35", (req, res) => {
